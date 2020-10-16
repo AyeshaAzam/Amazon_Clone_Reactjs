2233bd0d2ae27e8c2771c61f721f71b24eb3b338
Ayesha <ayesh76@msn.com>
2020-10-16 18:31:07 +0200

    Update firebase.js
    Just empty the firebase keys values

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBf5lKnt5wEpXPVlHip5JmbYL1f__5vxOE",
-  authDomain: "challenge-e2a0d.firebaseapp.com",
-  databaseURL: "https://challenge-e2a0d.firebaseio.com",
-  projectId: "challenge-e2a0d",
-  storageBucket: "challenge-e2a0d.appspot.com",
-  messagingSenderId: "1062384306116",
-  appId: "1:1062384306116:web:88bba8533c503c15330691",
-  measurementId: "G-1FKTE78DCQ",
+   apiKey: <your api key>,
+  authDomain: <your auth domain>,
+  databaseURL: <your database URL>,
+  projectId: <your project id>,
+  storageBucket: <your storage bucket>,
+  messagingSenderId: <your messaging sender id>,
+  appId: <your app id>,
+  measurementId: <your measurement id>,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
